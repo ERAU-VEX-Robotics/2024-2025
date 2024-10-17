89bf112eeecded7fffdf553115b7cbaf8f974523
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-10-17 16:58:03 -0700

    feat(small_bot_intake): Add header for small bot's intake
    Just the basic structure of a header file.

---
+++ small_bot/include/intake.h
@@ -0,0 +1,17 @@
+#ifndef INTAKE_H_
+#define INTAKE_H_
+
+/**
+ * @file intake.h
+ *
+ * @brief Function prototypes for controlling the intake
+ *
+ * @details This file contains declarations/prototypes for the functions used to
+ * control the intake. These functions serve as an API for the intake,
+ * abstracting away specific information.
+ */
+
+// Initializes internal intake variables
+void intake_init(void);
+
+#endif
