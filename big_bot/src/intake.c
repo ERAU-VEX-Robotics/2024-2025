c92886d2c118c45249fb87d78e17a337c28efbc0
Nikhil Dave <nikhilrdave2003@gmail.com>
2025-02-06 19:40:55 -0700

    chore(big_bot-intake,conveyor): Fix ports

--- big_bot/src/intake.c
+++ big_bot/src/intake.c
@@ -11,7 +11,7 @@
  * intake
  */
 
-static rgt_motor_group intake_motors = {4};
+static rgt_motor_group intake_motors = {2, 4};
 
 void intake_in() { rgt_mg_move(intake_motors, 127); }
 
@@ -19,8 +19,8 @@ void intake_out() { rgt_mg_move(intake_motors, -127); }
 
 void intake_opcontrol(controller_digital_e_t in_button,
                       controller_digital_e_t out_button) {
-  if (controller_get_digital(E_CONTROLLER_MASTER, in_button)) {
-    intake_in();
+	if (controller_get_digital(E_CONTROLLER_MASTER, in_button)) {
+		intake_in();
 	} else if (controller_get_digital(E_CONTROLLER_MASTER, out_button)) {
 		intake_out();
 	} else {
