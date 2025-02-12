57af4d4c8970ab269ebcd312d3c3a55891a40cae
Nikhil Dave <nikhilrdave2003@gmail.com>
2025-02-11 19:17:11 -0700

    chore(big_bot-piston): Use Ringtail pneumatics calls for piston
    Honestly I forgot I added pneumatics code in Ringtail.

--- big_bot/src/piston.c
+++ big_bot/src/piston.c
@@ -1,7 +1,7 @@
 #include "piston.h"
 
-#include "pros/adi.h"
 #include "pros/misc.h"
+#include "ringtail/pneumatics.h"
 
 #include <stdbool.h>
 
@@ -11,19 +11,11 @@
  * @brief Function implementations for big bot's piston
  */
 
-static const uint8_t piston_port = 'b';
-static bool piston_extended = false;
+static Rgt_Pneumatic_Piston p;
 
-void piston_init(void) { adi_port_set_config(piston_port, E_ADI_DIGITAL_OUT); }
+void piston_init(void) { p = rgt_pneumatic_init('b'); }
 
-void piston_update_state(void) {
-	adi_port_set_value(piston_port, piston_extended);
-}
-
-void piston_toggle(void) {
-	piston_extended = !piston_extended;
-	piston_update_state();
-}
+void piston_update_state(void) { rgt_pneumatic_toggle(&p); }
 
 void piston_opcontrol(controller_digital_e_t toggle_button) {
 	if (controller_get_digital_new_press(E_CONTROLLER_MASTER, toggle_button))
