11a745b1251ffb2c9e586600cd44c47421032484
Nikhil Dave <nikhilrdave2003@gmail.com>
2025-02-13 19:14:06 -0700

    fix(big_bot-piston): fix function names
    I'm dumb and deleted the piston_toggle function implementation

--- big_bot/src/piston.c
+++ big_bot/src/piston.c
@@ -15,7 +15,7 @@ static Rgt_Pneumatic_Piston p;
 
 void piston_init(void) { p = rgt_pneumatic_init('b'); }
 
-void piston_update_state(void) { rgt_pneumatic_toggle(&p); }
+void piston_toggle(void) { rgt_pneumatic_toggle(&p); }
 
 void piston_opcontrol(controller_digital_e_t toggle_button) {
 	if (controller_get_digital_new_press(E_CONTROLLER_MASTER, toggle_button))
